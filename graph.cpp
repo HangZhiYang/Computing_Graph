5c623d0b9c28796e189bd974ffad0961dccfa107
HangZhiYang <49983713+HangZhiYang@users.noreply.github.com>
2019-04-25 22:39:52 +0800

    map in graph.h and graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,7 +1,7 @@
 #include "graph.h"
 #include<iostream>
 #include <string>
-int Graph::getid(std::string str)
+/*int Graph::getid(std::string str)
 {
     for (int i = 0;i < used; ++i)
     {
@@ -9,60 +9,48 @@ int Graph::getid(std::string str)
             return i;
     }
     return -1;
-}
+}*/
 Graph::Graph(int sz)
 {
-    maxsize=sz;
-    nodes=new Node*[sz];
 }
 Graph::~Graph()
 {
-    delete[] nodes;
 }
 Node* Graph::getNamePointer(string str)
 {
-    for(int i=0; i<used; i++)
-    {
-        if(str==nodes[i]->name)
-            return nodes[i];
-    }
-    return nullptr;
+	if(nodes.count(str))return nodes[str];
+	else return nullptr;
 }
 Node* Graph::operator[](const string str)
 {
-    return getNamePointer(str);
+	return getNamePointer(str);
 }
 int Graph::push(Node* newnode)
 {
-    if(getid(newnode->name)!=-1)
-    {
-        std::cout<<"Same name!"<<std::endl;
-    }
-    if(used==maxsize)
-    {
-        std::cout<<"Max size!"<<std::endl;
-        return 1;
-    }
-    nodes[used]=newnode;
-    used++;
-    return 0;
+	if(nodes.count(newnode->name))
+	{
+		std::cout<<"Same name!"<<std::endl;
+		return 1;
+	}
+	nodes.insert(std::make_pair(newnode->name,newnode));
+	return 0;
 }
 void Graph::reset()
 {
-    for(int i=0; i<used; i++)
-    {
-        nodes[i]->reset();//����variable��constant 
-    }
+	for(auto iter = nodes.begin(); iter != nodes.end(); iter++)
+	{
+		iter->second->reset();
+	}
 }
 float Graph::eval(string nodename,int placeholdernum,string* placeholdernames,float* placeholdervalue)
 {
-    reset();
-    for(int i=0;i<placeholdernum;i++)
-        getNamePointer(placeholdernames[i])->setvalue(placeholdervalue[i]);
-        
-    return getNamePointer(nodename)->geteval();
+	reset();
+	for(int i=0; i<placeholdernum; i++)
+		getNamePointer(placeholdernames[i])->setvalue(placeholdervalue[i]);
+
+	return getNamePointer(nodename)->geteval();
 }
 void Graph::setvariable(string vname,float value)
 {
-    getNamePointer(vname)->setvalue(value);
+	getNamePointer(vname)->setvalue(value);
 }
--- graph.h
+++ graph.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <map>
 #include "node.h"
 #include "zeropn.h"
 #include "onepn.h"
@@ -9,16 +10,16 @@
 
 class Graph
 {
-	Node** nodes;
+	std::map<string,Node*> nodes;
 	int maxsize;
 	int used = 0;
-	int getid(std::string str);
+//	int getid(std::string str);
 	public:
 		Graph(int sz);
 		~Graph();
 		Node* getNamePointer(string str);
 		Node* operator[](const string str);
-		int push(Node* newnode);
+		int push(Node *newnode);
 		void reset();
 		float eval(string nodename,int placeholdernum,string* placeholdernames,float* placeholdervalue);
 		void setvariable(string vname,float value);
